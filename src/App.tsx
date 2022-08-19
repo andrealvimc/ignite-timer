77a87c5d58c831ac98aad7176c59408260ad48a9
André Alvim <andre@isportistics.com>
2022-08-19 12:32:01 -0300

    feat: configurações iniciais & styled components

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+
+export function App() {
+
+  return (
+    <><h1>teste</h1>
+
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button />
+    </>
+  )
+}
+
+
